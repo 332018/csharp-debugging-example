3f17ba23e921c614a35caa8d1b5db12a3f306332
332018 <72552532+332018@users.noreply.github.com>
2020-10-18 19:48:24 +0200

    Klasa Department

---
+++ Domaci/Domaci/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domaci_2
+{
+    class Department
+    {
+        public String Title;
+        public String Code;
+
+        public Department()
+        {
+            if (Code == "IT")
+                Title = "Informacione Tehnologije";
+            else if (Code == "RI")
+                Title = "Racunarska Tehnika";
+            else
+                Title = "Nista od navedenog";
+        }
+    }
+}
