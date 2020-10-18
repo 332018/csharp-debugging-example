a8b1aac12f5b23d0c20af5709195c33ac4c93bd9
332018 <72552532+332018@users.noreply.github.com>
2020-10-18 19:50:19 +0200

    Klasa Student

---
+++ Domaci/Domaci/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domaci_2
+{
+    class Student
+    {
+        public String Name;
+        public String IdNumber;
+        public Department Department;
+    }
+}
